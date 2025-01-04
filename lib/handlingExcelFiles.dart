eafd47d47e206d3bf2a1bb8f7ae0e9c5398bc28c
mazin <malkthere@gmail.com>
2025-01-05 00:23:07 +0300

    class01 - lesson01 creat excel file

---
+++ lib/handlingExcelFiles.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'package:flutter_excel/excel.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path/path.dart';
+
+
+
+class Excelfiles {
+  int _counter = 0;
+
+  //get selectedExcel => null;
+  var excel = Excel.createExcel();
+
+  void createexcelfile() async{
+    Sheet sheetObject = excel['فاتورة 01'];
+    var cell = sheetObject.cell(CellIndex.indexByString("A1"));
+    cell.value = 8; // dynamic values support provided;
+    List<String> data = ["Mr","’mazin", "Alkathiri"];
+    sheetObject.appendRow(data);
+    saveExcel();
+
+  }
+  saveExcel() async {
+    //request for storage permission
+    var res = await Permission.storage.request();
+    var fileBytes = excel.save();
+    //var directory = await getApplicationDocumentsDirectory();
+
+    File(join("/storage/emulated/0/Download/billNo03.xlsx"))
+      ..createSync(recursive: true)
+      ..writeAsBytesSync(fileBytes!);
+    //"/storage/emulated/0/Download/"  download folder address
+    //excel2.xlsx is the file name "feel free to change the file name to anything you want"
+
+
+
+  }
+
+
+
+}
