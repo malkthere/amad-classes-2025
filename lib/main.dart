b4d1f832b559f4fe5571e126fe07a685589a3575
mazin <malkthere@gmail.com>
2025-01-22 02:09:55 +0300

    class02 - lesson0 - working with PDF

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
-import 'dart:io';
-import 'package:file_picker/file_picker.dart';
-
+import 'package:amad/PdfPreviewPage.dart';
+import 'package:amad/pdfiles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_excel/excel.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:path/path.dart';
-import 'package:flutter/foundation.dart';
-import 'package:amad/handlingExcelFiles.dart';
 
 
 
@@ -23,21 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -48,16 +28,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -65,71 +35,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Textfiles fileobj = Textfiles();
+  final TextEditingController bookName = TextEditingController();
+  final TextEditingController author = TextEditingController();
+  final TextEditingController price = TextEditingController();
 
-
-
-
-  void _incrementCounter() async{
-
-  }
+  String text = "Your text to display below image";
 
   @override
   Widget build(BuildContext context) {
-    FilePickerResult? result;
-    File? file;
-    Excelfiles excelobj=new Excelfiles();
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () async {
-                // x=filecontaint as String;
-                try {
-                  result = await FilePicker.platform.pickFiles();
-                  // result = await FilePicker.platform.pickFiles();
-                  if (result != null) {
-                    if (!kIsWeb) {
-                      file = File(result!.files.single.path!);
-                      excelobj.readexcelfile(file);
-                    }
-                  } else {
-                    print("user cancled the picker");
-                    // User canceled the picker
-                  }
-
-                } catch (_) {}
-                super.setState(()  {
-
-                });
-              },
-              child: const Text('Pick a Text file File'),
+      appBar: AppBar(title: const Text("Sales Bill"),),
+      body: Column(
+        children: [
+          Image.asset("assets/phone.png"),
+          TextFormField(
+            controller: bookName,
+            decoration: InputDecoration(
+              labelText: 'Book Name',
+              border: OutlineInputBorder(),
             ),
-            ElevatedButton(
-              onPressed: () async {
-                excelobj.createexcelfile();
-              },
-              child: const Text('create an excel file'),
+          ),
+          SizedBox(height: 16),
+          TextFormField(
+            controller: author,
+            decoration: InputDecoration(
+              labelText: 'Author',
+              border: OutlineInputBorder(),
             ),
-
-
-          ],
-        ),
+          ),
+          SizedBox(height: 16),
+          TextFormField(
+            controller: price,
+            decoration: InputDecoration(
+              labelText: 'Price',
+              border: OutlineInputBorder(),
+             // keyboardType: TextInputType.number,
+            ),
+          ),
+          Text(text),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: (){
+          final bookDetails = [
+            bookName.text,
+            author.text,
+            price.text,
+          ];
+          Navigator.of(context).push(MaterialPageRoute(builder: (context){
+            return  PdfPreviewPage(bookDetails);
+          }));
+        },
+        child: const Icon(Icons.picture_as_pdf_sharp),
+      ),
     );
   }
-}
-
-
+  }
 
